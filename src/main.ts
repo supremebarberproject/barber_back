c947c5e2c503a378bc3f0ee8f1937d6258f4e049
Artifexted <artifexted@gmail.com>
2025-02-09 20:56:10 -0300

    Instalacion de Swagger y algunas dependencias

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,21 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+    .setTitle("Barber Back")
+    .setDescription("API para la barberia de Don Julio")
+    .setVersion("1.0")
+    .build()
+
+  const documentFactory = () => SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup("API", app, documentFactory, {
+    customSiteTitle: "Barberia Don Julio",
+  })
+
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
